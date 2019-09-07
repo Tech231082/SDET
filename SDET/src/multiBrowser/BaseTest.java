b000f45e56cb1ea312ecb9e10ae1d6f9073b7eae
Sushila Goyat <sush.rinwa@gmail.com>
2019-09-06 20:30:29 -0600

    commit

--- SDET/src/multiBrowser/BaseTest.java
+++ SDET/src/multiBrowser/BaseTest.java
@@ -2,30 +2,39 @@ package multiBrowser;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
-import com.gargoylesoftware.htmlunit.javascript.configuration.BrowserName;
+
 
 public class BaseTest {
+	//public static  browser;
 	public static WebDriver driver;
-	@Parameters("browserName")
-	@BeforeTest
-	public void setUp() {
 	
-				
-		System.setProperty("webdriver.chrome.driver", "C:/mytools/chromedriver.exe");
+	
+	
+	@Test
+	@Parameters({"browser"})
+	public void test(@Optional("browser")) {
+		if(browser.equals("chrome"))	{
+			System.setProperty("webdriver.chrome.driver", "C:/mytools/chromedriver.exe");
+			
+			 driver=new ChromeDriver();
+		}
+		else {
+			System.setProperty("webdriver.gecko.driver", "C:/mytools/geckodriver.exe");
+			
+			driver=new FirefoxDriver();
+		}
 		
-		WebDriver driver=new ChromeDriver();
 	}
 	
-	@AfterTest
-	public void tearDown() {
-		driver.quit();
-	}
+}	
 	
 
-}
+
